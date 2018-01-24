4b26d3d123338614f02d0c2e7d243d98d19a8b58
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-01-23 21:57:55 -0500

    updated database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180121204005) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "answer_text"
     t.text "name"
