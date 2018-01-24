0cb9c18625bdf4cea41ff639affe6b0ec4b3fc78
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-01-23 21:41:46 -0500

    first backend deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ Question.create(title: "Where can I find the bathroom code", name:"Sam Goldstein
 Question.create(title: "How long is the onboarding process?", name:"Ian Lowe")
 Answer.create(answer_text:"It's posted by the front desk", name:"Betty White", question_id: 1, like: 1)
 Answer.create(answer_text:"It's on the slack channel", name:"Martha May", question_id: 1, like: 1)
+Question.create(title: "What's the best process to escalate a bug?", name:"Ian Lowe")
+Question.create(title: "What's your advice for prioritizing integration requests?", name:"Ian Lowe")
+Question.create(title: "What's your advice with regard to working effectively with the sales team?", name:"Ian Lowe")
+Question.create(title: "Is there a protocol for documenting integration requests? If so what is it?", name:"Ian Lowe")
+Question.create(title: "Do you have advice for a new Customer Success Engineer?", name:"Ian Lowe")
+
+
+
+
+
+
+
+
+
